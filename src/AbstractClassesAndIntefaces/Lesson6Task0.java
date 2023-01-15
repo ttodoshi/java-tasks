ebd874c5f1af48b332339b6a14c1726c377163cc
todoshi <ttodoshi@users.noreply.github.com>
2023-01-15 12:15:55 +0300

    lesson 6 task 1

---
+++ src/AbstractClassesAndIntefaces/Lesson6Task0.java
@@ -0,0 +1,37 @@
+package AbstractClassesAndIntefaces;
+
+public class Lesson6Task0 {
+    public static void main(String[] args) {
+        System.out.println(new CelsiusConverter().convert(22.5));
+        System.out.println(new KelvinsConverter().convert(22.5));
+        System.out.println(new FahrenheitConverter().convert(22.5));
+    }
+}
+
+abstract class BaseConverter {
+    protected abstract double convert(double celsius);
+}
+
+class CelsiusConverter extends BaseConverter {
+
+    @Override
+    protected double convert(double celsius) {
+        return celsius;
+    }
+}
+
+class KelvinsConverter extends BaseConverter {
+
+    @Override
+    protected double convert(double celsius) {
+        return celsius + 273.15;
+    }
+}
+
+class FahrenheitConverter extends BaseConverter {
+
+    @Override
+    protected double convert(double celsius) {
+        return celsius * 9 / 5 + 32;
+    }
+}

3ada21b96c556609263a6b0156d1103365dc9f02
Александр Шешин <sheshin.alexanderkpc2052020@gmail.com>
2023-02-05 20:02:04 +0300

    almost redone binary search tree

--- src/recursion/Lesson3Task2/BinarySearchTree.java
+++ src/recursion/Lesson3Task2/BinarySearchTree.java
@@ -1,64 +1,80 @@
 package recursion.Lesson3Task2;
 
 public class BinarySearchTree {
-    private final Node head;
-    private final Node[] nodes;
-
-    public BinarySearchTree(int numOfNodes, long headNode) {
-        this.nodes = new Node[numOfNodes];
-        this.head = new Node(headNode);
-        this.nodes[Node.k] = this.head;
-    }
+    private Node root;
 
     public void add(long value) {
-        add(this.head, value);
+        add(this.root, value);
     }
 
-    private void add(Node head, long value) {
-        if (head.left == null && head.compareTo(value)) {
-            head.left = new Node(value);
-            nodes[Node.k - 1] = head.left;
-        } else if (head.right == null && !head.compareTo(value)) {
-            head.right = new Node(value);
-            nodes[Node.k - 1] = head.right;
-        } else if (head.compareTo(value)) {
-            add(head.left, value);
-        } else {
-            add(head.right, value);
+    private void add(Node currentNode, long value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+        }
+        else {
+//            Node parent = currentNode;
+            while (currentNode != null) {
+                if (currentNode.isLessThan(value) && currentNode.right != null) {
+                    currentNode = currentNode.right;
+                }
+                else if (currentNode.isLessThan(value) && currentNode.right == null) {
+                    currentNode.right = newNode;
+                    return;
+                }
+                else if (!currentNode.isLessThan(value) && currentNode.left != null) {
+                    currentNode = currentNode.left;
+                }
+                else {
+                    currentNode.left = newNode;
+                    return;
+                }
+            }
+
         }
     }
 
     public Node get(long value) {
-        return get(this.head, value);
+        return get(this.root, value);
     }
 
-    private Node get(Node head, long value) {
-        if (head == null) {
-            return null;
-        } else if (head.data == value) {
-            return head;
+    private Node get(Node currentNode, long value) {
+        if (currentNode == null) {
+            throw new RuntimeException("Not found");
+        } else if (currentNode.equals(value)) {
+            return currentNode;
         } else {
-            if (head.compareTo(value)) {
-                return get(head.left, value);
-            } else {
-                return get(head.right, value);
+            if (currentNode.isLessThan(value)) {
+                return get(currentNode.right, value);
+            }
+            else {
+                return get(currentNode.left, value);
             }
         }
     }
 
     static class Node {
-        private final long data;
-        private Node left;
-        private Node right;
-        private static int k = 0;
+        final long data;
+        Node left;
+        Node right;
+
+//        Node parent;
 
         public Node(long data) {
             this.data = data;
-            k++;
         }
 
-        private boolean compareTo(long value) {
-            return this.data >= value;
+        private boolean isLessThan(long value) {
+            return this.data < value;
+        }
+
+        public boolean equals(long value) {
+            return this.data == value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(data);
         }
     }
 }
